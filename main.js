0597807c08e79d832ed41092b35a032ff578d8d4
Camille-Durand <camillefanguin@gmail.com>
2023-11-16 16:19:46 +0100

    Ajout fichiers HTML & JS

---
+++ main.js
@@ -0,0 +1,58 @@
+const usersHuman = [{
+        type: "humain",
+        name: "John Doe",
+        email: "j.smith@gmail.com",
+        age: 25
+    },
+    {
+        type: "humain",
+        name: "Jane Smith",
+        email: "ja.doe@sfr.fr",
+        age: 5
+    },
+    {
+        type: "humain",
+        name: "Le Vénérable",
+        email: "levy@gmail.com",
+        age: 500
+    }
+];
+
+const usersPet = [{
+        type: "animal de compagnie",
+        espece: "chien",
+        name: "Rox",
+        age: 7,
+        propriétaire: "John Doe"
+    },
+    {
+        type: "animal de compagnie",
+        espece: "renard",
+        name: "Roukie",
+        age: 300,
+        propriétaire: "Le Vénérable"
+    }
+];
+
+const usersXeno = [{
+        type: "Xeno",
+        espece: "Krogan",
+        name: "Wrex",
+        menace: "Rouge",
+        age: 45
+    },
+    {
+        type: "Xeno",
+        espece: "Turien",
+        name: "Garrus",
+        menace: "Vert",
+        age: 35
+    },
+    {
+        type: "Xeno",
+        espece: "Asari",
+        name: "Liara",
+        menace: "ULTRA Rouge",
+        age: 25
+    }
+];
